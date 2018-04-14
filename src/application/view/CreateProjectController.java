45c05dab5ff25c2fbb888a05d0cd8a0d2b684c39
Daniel Russom <daniel.russom00@gmail.com>
2018-04-14 02:55:24 +0100

    GUI Display
    -Project menu now displayed on startup
    -Menu root layout added
    -Create project display added
    -Added and linked buttons to placeholder functions
    -Updated project menu display

---
+++ src/application/view/CreateProjectController.java
@@ -0,0 +1,20 @@
+package application.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class CreateProjectController {
+
+	@FXML
+	private TextField tfProjectName;
+	
+	@FXML
+	private void btnCreateProject() {
+		
+	}
+	
+	@FXML
+	private void btnReturn() {
+		
+	}
+}
---
+++ src/application/view/ProjectMenuController.java
@@ -0,0 +1,21 @@
+package application.view;
+
+import javafx.fxml.FXML;
+
+public class ProjectMenuController {
+
+	@FXML
+	private void btnCreateNewProject() {
+		
+	}
+
+	@FXML
+	private void btnOpenProject() {
+		
+	}
+	
+	@FXML
+	private void btnReturn() {
+		
+	}
+}
