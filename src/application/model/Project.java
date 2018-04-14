25791210c8731051db3751975dc3428b43bfd3b2
Daniel Russom <daniel.russom00@gmail.com>
2018-04-14 03:50:28 +0100

    Project Description
    -Added description to project
    -Added project constructor with optional description
    -Updated create project display

--- src/application/model/Project.java
+++ src/application/model/Project.java
@@ -6,20 +6,35 @@ public class Project {
 	static private int idCounter = 0;
 	private int id;
 	private String name;
+	private String description;
 	private ArrayList<Element> elements;
-	
+
 	public Project(String name) {
 		id = idCounter;
 		this.name = name;
 		idCounter++;
 	}
 
-	/* (non-Javadoc)
+	public Project(String name, String description) {
+		id = idCounter;
+		this.name = name;
+		this.description = description;
+		idCounter++;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see java.lang.Object#toString()
 	 */
 	@Override
 	public String toString() {
-		return "Project " + id + ": [name = " + name + ", elements = " + elements + "]";
+		String toString = "Project " + id + ": [name = " + name + ", ";
+		if (description != null && !description.isEmpty()) {
+			  toString = toString + "description = " + description + ", ";
+			}
+		 toString = toString + "elements = " + elements + "]";
+		 return toString;
 	}
 
 }
--- src/application/view/CreateProjectController.java
+++ src/application/view/CreateProjectController.java
@@ -3,6 +3,7 @@ package application.view;
 import application.Main;
 import application.model.Project;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 public class CreateProjectController {
@@ -12,10 +13,19 @@ public class CreateProjectController {
 	private TextField tfProjectName;
 
 	@FXML
+	private TextArea taProjectDescription;
+
+	@FXML
 	private void btnCreateProject() {
 		String projectName = tfProjectName.getText();
-		Project newProject = new Project(projectName);
-		System.out.println(newProject.toString());
+		String description = taProjectDescription.getText();
+		Project newProject;
+		if ((description != null) && (description.trim().length() > 0)) {
+			newProject = new Project(projectName, description);
+		} else {
+			newProject = new Project(projectName);
+		}
+		System.out.println("Project Created - " + newProject.toString());
 		main.showProjectMenu();
 	}
 
